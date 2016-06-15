85c6bde6c088812bef24a2a148582391f3e201aa
jordanbudi <jordanbudi@gmail.com>
2016-06-15 10:13:01 -0400

    challenge-2, STRETCH, and image updates

---
+++ metgala_invitation.rb
@@ -0,0 +1,3 @@
+#Code Here!
+
+gala_invitation = "The Museum of Modern Art invites you to their annual gala on Sunday the 22nd of May 2015. Festivities will be held at the MoMA at 11 W 53rd St, New York, NY 10019. See you then!"
