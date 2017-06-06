2a3f976a643c5e27682f1dd8851438767c59d006
Rachel Pierron <rachelopierron@gmail.com>
2017-06-06 18:14:30 +0000

    invitation lab

--- invitation.rb
+++ invitation.rb
@@ -1,1 +1,15 @@
 #Code Here!
+wrong_fact = "Karlie was born in Kansas City, Missouri"
+right_fact = wrong_fact.gsub("Kansas City", "St. Louis")
+puts wrong_fact
+puts right_fact
+wrong_fact = "Karlie has 5 brothers"
+true_fact = wrong_fact.gsub("brothers", "sisters").gsub("5", "3")
+puts true_fact
+name_of_show = "The Tonight Show"
+show_host = "Jimmy Fallon"
+puts "In May, Karlie was on #{name_of_show} with #{show_host}!"
+wrong_gala_invitation = "The Museum of Modern Art invites you to their annual gala on Sunday the 22nd of May 2015. Festivities will be held at the MoMA at 11 W 53rd St, New York, NY 10019
+. See you then!"
+gala_invitation = wrong_gala_invitation.gsub("Museum of Modern Art", "Metropolitan Museum").gsub("Sunday the 22nd of May 2015", "Saturday May 13th, 2017")
+puts gala_invitation
--- metgala_invitation.rb
+++ metgala_invitation.rb
@@ -1,3 +1,16 @@
 #Code Here!
 
-gala_invitation = "The Museum of Modern Art invites you to their annual gala on Sunday the 22nd of May 2015. Festivities will be held at the MoMA at 11 W 53rd St, New York, NY 10019. See you then!"
+day = "Friday".capitalize
+date = "26th"
+year = "2017"
+gala_invitation = "The Museum of Modern Art invites you to their annual gala on #{day} the #{date} of May #{year}. Festivities will be held at the MoMA at 11 W 53rd St, New York, NY 10019. See you then!"
+puts gala_invitation
+puts "to know the day, enter your favorite color"
+favorite_color = gets
+puts "The Gala is on Friday"
+puts "to know the location, enter your favorite animal"
+favorite_animal = gets
+puts "The Gala is at The Museum of Modern Art"
+puts "to know the date, enter your favorite song"
+favorite_song = gets
+puts "May 26th, 2017"
